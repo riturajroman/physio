1b3ad34885d12f7a0df1d858a01350e1b9bb2e8a
Ritu Raj <145446035+riturajroman@users.noreply.github.com>
2024-05-02 15:52:08 +0530

    elderly meta change

--- app/layout.js
+++ app/layout.js
@@ -4,8 +4,8 @@ import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  title: "Physio Registeration App",
-  description: "Physio Registeration App",
+  title: "Elderly Registeration App",
+  description: "Elderly Registeration App",
 };
 
 export default function RootLayout({ children }) {
